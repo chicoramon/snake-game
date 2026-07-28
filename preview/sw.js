b5a03e1dc44fe1a29ff2d7ccb399cc6466eab1ae
Ramy Moussally <ramy.moussally@dar.com>
2026-07-28 16:13:15 +0300

    Deploy Snake game preview - 2026-07-28 13:13:15 UTC

--- preview/sw.js
+++ preview/sw.js
@@ -25,7 +25,7 @@ const PRECACHE_ASSETS = [
   "./assets/icons/icon-192.png",
   "./assets/icons/icon-512-maskable.png",
   "./assets/icons/icon-512.png",
-  "./assets/index-COElhyqr.js",
+  "./assets/index-47a1G9JS.js",
   "./assets/index-Gn-M_qea.css",
   "./assets/manifest-BkglArjv.webmanifest",
   "./assets/social/og-image.jpg",
